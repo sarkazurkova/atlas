6c990022eb09fc0bd681fbe26277f5607c09d0d3
sarkazurkova <120708285+sarkazurkova@users.noreply.github.com>
2024-05-02 12:57:55 +0200

    uprava

--- atlas.js
+++ atlas.js
@@ -5,15 +5,16 @@ fetch('https://restcountries.com/v3.1/region/europe')
     .then((data) => {
         console.log(data);
         data.forEach(stat => {
-            let blockCountry = `<div class="col-xl-1 col-lg-2 col-md-4 col-sm-6">
+            let blockCountry = `<div class="col-xl-1 col-lg-2 col-md-4 col-sm-6 my-5">
         <div class="card">
             <a href="${stat.maps.googleMaps}">
             <img class="card-img-top" src="${stat.flags.png}" alt="${stat.name.official}" />
             </a>
             <div class="card-body">
-                <h4 class="card-title">${stat.translations.ces.common}</h4>
-                <p class="card-text">Pocet obyvatel: ${stat.population} <br>
-                Rozloha: ${stat.area} km<sup>2</sup></p>
+                <h5 class="card-title"><b>${stat.translations.ces.common}</b></h5>
+                <p class="card-text"><b>Počet obyvatel:</b><br> ${stat.population} <br>
+                <b>Rozloha:</b><br> ${stat.area} km<sup>2</sup></p>
+               
             </div>
         </div>
         
@@ -23,3 +24,5 @@ fetch('https://restcountries.com/v3.1/region/europe')
         
         //staty.innerHTML= data[9].translations.ces.common;
     });
+
+    
