bded1af2a01d3dfad0336fd070306e9d68067c16
sarkazurkova <120708285+sarkazurkova@users.noreply.github.com>
2024-05-07 13:03:49 +0200

    Add files via upload

--- atlas.js
+++ atlas.js
@@ -1,11 +1,15 @@
 const staty = document.getElementById('staty');
 
+
+
+//<button type="button" onclick="cast=europe">Evropa</button>
+
 fetch('https://restcountries.com/v3.1/region/europe')
     .then((response) => response.json())
     .then((data) => {
         console.log(data);
         data.forEach(stat => {
-            let blockCountry = `<div class="col-xl-1 col-lg-2 col-md-4 col-sm-6 my-5">
+            let blockCountry = `<div class="col-xl-2 col-lg-2 col-md-4 col-sm-6 my-5">
         <div class="card">
             <a href="${stat.maps.googleMaps}">
             <img class="card-img-top" src="${stat.flags.png}" alt="${stat.name.official}" />
@@ -14,7 +18,7 @@ fetch('https://restcountries.com/v3.1/region/europe')
                 <h5 class="card-title"><b>${stat.translations.ces.common}</b></h5>
                 <p class="card-text"><b>Počet obyvatel:</b><br> ${stat.population} <br>
                 <b>Rozloha:</b><br> ${stat.area} km<sup>2</sup></p>
-               
+            
             </div>
         </div>
         
