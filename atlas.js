c7adc578ce56fa11efd9d509e65902a6fc3f5dcd
sarkazurkova <120708285+sarkazurkova@users.noreply.github.com>
2024-05-13 20:22:31 +0200

    dokonceni

--- atlas.js
+++ atlas.js
@@ -1,7 +1,7 @@
 const staty = document.getElementById('staty');
-
 function showCountriesByContinent(continent) {
-    staty.innerHTML = '';
+    staty.innerHTML = ''; // Vymažeme aktuální obsah
+
 
 //<button type="button" onclick="cast=europe">Evropa</button>
 
@@ -16,7 +16,7 @@ fetch(`https://restcountries.com/v3.1/region/${continent}`)
             <img class="card-img-top" src="${stat.flags.png}" alt="${stat.name.official}" />
             </a>
             <div class="card-body">
-            <h5 class="card-title"><a href="#" class="country-link"><b>${stat.translations.ces.common}</b></a></h5>
+                <h5 class="card-title"><a href="#" class="country-link"><b>${stat.translations.ces.common}</b></a></h5>
                 <p class="card-text"><b>Počet obyvatel:</b><br> ${stat.population} <br>
                 <b>Rozloha:</b><br> ${stat.area} km<sup>2</sup></p>
             
@@ -26,21 +26,21 @@ fetch(`https://restcountries.com/v3.1/region/${continent}`)
     </div>`;
     staty.innerHTML += blockCountry;
     });
-        
-        //staty.innerHTML= data[9].translations.ces.common;
-    });
-    const countryLinks = document.querySelectorAll('.country-link');
-    countryLinks.forEach(link => {
-        link.addEventListener('click', function(event) {
-            event.preventDefault(); // Zabraňuje výchozímu chování odkazu
-            const index = Array.from(countryLinks).indexOf(link);
-            const selectedStat = data[index];
-            showModalWithCountryInfo(selectedStat.translations.ces.common, selectedStat.population, selectedStat.area);
-        });
-    });
 
+    const countryLinks = document.querySelectorAll('.country-link');
+            countryLinks.forEach(link => {
+                link.addEventListener('click', function(event) {
+                    event.preventDefault(); // Zabraňuje výchozímu chování odkazu
+                    const index = Array.from(countryLinks).indexOf(link);
+                    const selectedStat = data[index];
+                    showModalWithCountryInfo(selectedStat.translations.ces.common, selectedStat.population, selectedStat.area, 
+                    selectedStat.capital, selectedStat.tld);
+                });
+});   
+});
 }
 
+    // Funkce pro automatické zobrazení států pro kontinent Evropa po načtení stránky
 document.addEventListener('DOMContentLoaded', function() {
     showCountriesByContinent('europe');
 });
@@ -51,31 +51,33 @@ document.querySelectorAll('.dropdown-item').forEach(item => {
         showCountriesByContinent(continent);
     });
 });
-// Funkce pro zobrazení modálního okna s informacemi o státu
-function showModalWithCountryInfo(name, flagUrl, population, area) {
-    const modalTitle = document.getElementById('modal-title');
-    const modalFlag = document.getElementById('modal-flag');
-    const modalInfo = document.getElementById('modal-info');
-
-    modalTitle.textContent = name;
-    modalFlag.src = flagUrl;
-    modalInfo.innerHTML = `Počet obyvatel: ${population}<br>Rozloha: ${area} km<sup>2</sup>`;
-
-    // Zobrazení modálního okna
-    const modal = document.getElementById('myModal');
-    modal.style.display = 'block';
-
-    // Přidání událostního posluchače pro zavření modálního okna po kliknutí na ikonu "X"
-    const closeBtn = document.getElementsByClassName('close')[0];
-    closeBtn.addEventListener('click', function() {
-        modal.style.display = 'none';
-    });
-
-    // Přidání událostního posluchače pro zavření modálního okna po kliknutí mimo okno
-    window.addEventListener('click', function(event) {
-        if (event.target === modal) {
-            modal.style.display = 'none';
+        
+        function showModalWithCountryInfo(name, population, area, capital, tld) {
+            const modalTitle = document.getElementById('modal-title');
+            const modalInfo = document.getElementById('modal-info');
+        
+            modalTitle.textContent = name;
+           
+            modalInfo.innerHTML = `Počet obyvatel: ${population}<br>Rozloha: ${area} km<sup>2</sup>
+            <br>Hlavní město: ${capital}  <br>Www doména: ${tld}`;
+        
+            // Zobrazení modálního okna
+            const modal = document.getElementById('myModal');
+            modal.style.display = 'block';
+        
+            // Přidání událostního posluchače pro zavření modálního okna po kliknutí na ikonu "X"
+            const closeBtn = document.getElementsByClassName('close')[0];
+            closeBtn.addEventListener('click', function() {
+                modal.style.display = 'none';
+            });
+        
+            // Přidání událostního posluchače pro zavření modálního okna po kliknutí mimo okno
+            window.addEventListener('click', function(event) {
+                if (event.target === modal) {
+                    modal.style.display = 'none';
+                }
+            });
         }
-    });
-}
+          
+
     
